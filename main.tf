1acd3d405d2b321b789eada6e316ab493fc3c435
nclose-kieran <kieran@nclose.com>
2023-02-08 15:11:01 +0200

    adjust org

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "kieran-nclose-terraform-cicd"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-terraform-cicd"
     }
   }
 }
